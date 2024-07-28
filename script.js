d9f7fbf70e506573def489859750e1a3aabcc4c5
sanvi <sanvis0184@gmail.com>
2024-07-29 00:38:01 +0530

    add all completed files till yet

---
+++ script.js
@@ -0,0 +1,106 @@
+var timeout;
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+const navMenu = document.getElementById("nav-ul");
+const menuButton = document.getElementById("menunav");
+
+menuButton.addEventListener("click", function () {
+    if (navMenu.classList.contains("show")) {
+        navMenu.classList.remove("show");
+        menuButton.innerHTML = "<h3>Menu+</h3>";
+    } else {
+        navMenu.classList.add("show");
+        menuButton.innerHTML = `<i class="ri-close-line"></i>`;
+    }
+});
+
+
+function circleChaptaKaro() {
+    // define default scale value
+    var xscale = 1;
+    var yscale = 1;
+
+    var xprev = 0;
+    var yprev = 0;
+
+    window.addEventListener("mousemove", function (dets) {
+        clearTimeout(timeout);
+
+        xscale = gsap.utils.clamp(0.6, 1.2, dets.clientX - xprev);
+        yscale = gsap.utils.clamp(0.6, 1.2, dets.clientY - yprev);
+
+        xprev = dets.clientX;
+        yprev = dets.clientY;
+
+        circleMouseFollower(xscale, yscale);
+
+        timeout = setTimeout(function () {
+            document.querySelector(
+                "#minicircle"
+            ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+        }, 100);
+    });
+}
+
+function circleMouseFollower(xscale, yscale) {
+    window.addEventListener("mousemove", function (dets) {
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
+    });
+}
+function firstPageAnim() {
+    var tl = gsap.timeline();
+
+    tl.from(".nav", {
+        y: "-10",
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut,
+    })
+        .to(".boundingelem", {
+            y: 0,
+            ease: Expo.easeInOut,
+            duration: 2,
+            delay: -1,
+            stagger: 0.2,
+        })
+        .from("#section-1-footer", {
+            y: -10,
+            opacity: 0,
+            duration: 1.5,
+            delay: -1,
+            ease: Expo.easeInOut,
+        });
+}
+circleChaptaKaro();
+circleMouseFollower();
+firstPageAnim();
+
+document.querySelectorAll(".proj").forEach(function (elem) {
+    var rotate = 0;
+    var diffrot = 0;
+
+    elem.addEventListener("mouseleave", function (dets) {
+        gsap.to(elem.querySelector("img"), {
+            opacity: 0,
+            ease: Power3,
+            duration: 0.5,
+        });
+    });
+    elem.addEventListener("mousemove", function (dets) {
+        var diff = dets.clientY - elem.getBoundingClientRect().top; /*
+        getBoundingClientRect() is a JavaScript method that returns the size of an element and its position relative to the viewport. This method provides an easy way to get the position and size of an element for tasks such as collision detection, animations, and other positioning logic.*/
+        diffrot = dets.clientX - rotate;
+        rotate = dets.clientX;
+        gsap.to(elem.querySelector("img"), {
+            opacity: 1,
+            ease: Power3,
+            top: diff,
+            left: dets.clientX,
+            rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5)
+        });
+    });
+});
